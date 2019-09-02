af385e9f5fd4f465306ccd7545005dac00f14f70
Anna <8pipopolam@gmail.com>
2019-09-02 09:38:33 +0300

    11

---
+++ DataRange/src/DataRange.kt
@@ -0,0 +1,9 @@
+class DateRange(val start: MyDate, val endInclusive: MyDate){
+    operator fun contains(d: MyDate):Boolean{
+        return  compare(start, d) && compare(d,endInclusive)
+    }
+}
+fun compare(date1: MyDate, date2: MyDate) = date1 < date2
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    return date in DateRange(first, last)
+}
